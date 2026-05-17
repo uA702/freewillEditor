2eff2e6d1e5961d06fec29305ca9832fd5071d89
uA702 <67120946+uA702@users.noreply.github.com>
2026-05-17 18:23:58 +0200

    Add contranst and brightness to mono channels

--- src/app/pipeline.py
+++ src/app/pipeline.py
@@ -177,6 +177,8 @@ fx_b2_roi = fx.ROIEffect()
 
 # --- Channel 3 Layers ---
 fx_m1_mono     = fx.MonochromeEffect()
+fx_m1_bright   = fx.BrightnessEffect()
+fx_m1_contrast = fx.ContrastEffect()
 fx_m1_edges    = fx.EdgeDetectionEffect()
 fx_m1_thresh   = fx.ThresholdingEffect()
 fx_m1_cmap     = fx.ColorMappingEffect()
@@ -187,6 +189,8 @@ fx_m1_roi = fx.ROIEffect()
 
 # --- Channel 4 Layers ---
 fx_m2_mono     = fx.MonochromeEffect()
+fx_m2_bright   = fx.BrightnessEffect()
+fx_m2_contrast = fx.ContrastEffect()
 fx_m2_edges    = fx.EdgeDetectionEffect()
 fx_m2_thresh   = fx.ThresholdingEffect()
 fx_m2_cmap     = fx.ColorMappingEffect()
@@ -231,7 +235,8 @@ stage_channel_2.add_layers([
 # --- MASTER STAGE 3: MONOCHROME CHANNEL 1 ---
 stage_channel_3 = PipelineStage("Channel 3: Mono Glitch Alpha", standard_linear_processor)
 stage_channel_3.add_layers([
-    fx_m1_mono, fx_m1_thresh, fx_m1_edges, 
+    fx_m1_mono, fx_m1_bright, fx_m1_contrast,
+    fx_m1_thresh, fx_m1_edges, 
     fx_m1_blur, fx_m1_echo, fx_m1_tblur, 
     fx_m1_cmap, fx_m1_roi
 ])
@@ -239,7 +244,8 @@ stage_channel_3.add_layers([
 # --- MASTER STAGE 4: MONOCHROME CHANNEL 2 ---
 stage_channel_4 = PipelineStage("Channel 4: Mono Glitch Beta", standard_linear_processor)
 stage_channel_4.add_layers([
-    fx_m2_mono, fx_m2_thresh, fx_m2_edges,
+    fx_m2_mono, fx_m2_bright, fx_m2_contrast,
+    fx_m2_thresh, fx_m2_edges,
     fx_m2_blur, fx_m2_echo, fx_m2_tblur,
     fx_m2_cmap, fx_m2_roi
 ])
